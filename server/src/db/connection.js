b42a30db2769f3a06c138ffa18fa09e0b5556916
Luis Canales <canalesluis9@gmail.com>
2023-12-28 21:56:07 -0300

    first commit

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,2 +1,20 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 
+const sequelize = new Sequelize(process.env.DB, {
+  logging: false,
+  native: false,
+  ssl: true,
+});
+
+const dbConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("DB Connect");
+  } catch (error) {
+    throw new Error("Fail DB Connect");
+  }
+};
+
+module.exports = { dbConnection };
 
