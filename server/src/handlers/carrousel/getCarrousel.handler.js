f491eff998be833b04bea131fb8026f4e5997d82
Luis Canales <canalesluis9@gmail.com>
2025-09-02 02:02:12 -0300

    console log)

--- server/src/handlers/carrousel/getCarrousel.handler.js
+++ server/src/handlers/carrousel/getCarrousel.handler.js
@@ -5,6 +5,7 @@ module.exports = async (req, res) => {
     const carrousel = await getCarrouselController();
     res.status(200).json(carrousel);
   } catch (error) {
+    console.log(error);
     res.status(400).json({ error: error.message });
   }
 };
